513e8a65694055173a2193bf7b2b3adcae5310e8
Jason Longshore <longshorej@gmail.com>
2018-01-09 15:12:09 -0600

    Rename play/lagom endpoints to http

--- service-discovery-lagom13-java/src/main/java/com/lightbend/rp/servicediscovery/lagom/javadsl/LagomServiceLocator.java
+++ service-discovery-lagom13-java/src/main/java/com/lightbend/rp/servicediscovery/lagom/javadsl/LagomServiceLocator.java
@@ -40,7 +40,7 @@ public class LagomServiceLocator extends CircuitBreakingServiceLocator {
     @Override
     public CompletionStage<Optional<URI>> locate(String name, Descriptor.Call<?, ?> serviceCall) {
         return com.lightbend.rp.servicediscovery.javadsl.ServiceLocator
-                .lookupOne(name, "lagom-http-api", actorSystem)
+                .lookupOne(name, "http", actorSystem)
                 .thenCompose(service -> service.isPresent() ? CompletableFuture.completedFuture(service) : com.lightbend.rp.servicediscovery.javadsl.ServiceLocator.lookupOne(name, actorSystem))
                 .thenApply(service -> service.map(Service::uri));
     }
